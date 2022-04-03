ca8b356c46fa8873d5e4fd288b406c040cc5a6e6
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-04-03 20:04:55 -0300

    se crea el listado de personejes

--- cursoAngular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ cursoAngular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -11,9 +11,20 @@ interface Personaje {
 })
 export class MainPageComponent implements OnInit {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
   constructor() { }
@@ -23,6 +34,15 @@ export class MainPageComponent implements OnInit {
 
 
   agregar() {
+    //validacion si esta vacio la propieda nombre
+    if (this.nuevo.nombre.trim().length === 0) { return; }// si no tiene nada 'nombre', pues que salimos del metodo con return
+
+    this.personajes.push(this.nuevo);//agregamos a la lista
+    //purgamos el objeto
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
     console.log(this.nuevo);
   }
 
