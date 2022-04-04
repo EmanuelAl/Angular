1e9e9ebb75ad00111e921dacd1a7fcd416a7ff89
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-04-04 11:46:40 -0300

    uso del @output y EventEmitter para desde el hijo pasar inf al padre

--- cursoAngular/01-bases/src/app/dbz/agregar/agregar.component.ts
+++ cursoAngular/01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
@@ -7,25 +8,25 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class AgregarComponent {
 
-  @Input() personajes: Personaje[] = [];
   @Input() nuevo: Personaje = {
     nombre: '',
     poder: 0
   }
+//creamos propiedad de tipo evento que emitira un Personaje en el componente padre
+  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
 
   agregar() {
     //validacion si esta vacio la propieda nombre
     if (this.nuevo.nombre.trim().length === 0) { return; }// si no tiene nada 'nombre', pues que salimos del metodo con return
 
-    this.personajes.push(this.nuevo);//agregamos a la lista
     console.log(this.nuevo);
+    this.onNuevoPersonaje.emit(this.nuevo);//emitimos el personaje cuando este evento sea llamado
     //purgamos el objeto
     this.nuevo = {
       nombre: '',
       poder: 0
     }
     
-    console.log(this.personajes);
   }
 
 
