cea0c3f7f3536836e9758da840ae4e159fb1f09b
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-04-06 00:38:19 -0300

    se elimina codigo que no se esta utilizando en cada componente

--- cursoAngular/01-bases/src/app/dbz/personajes/personajes.component.ts
+++ cursoAngular/01-bases/src/app/dbz/personajes/personajes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { DbzService } from '../services/dbz.service';
 //componente hijo de main-page
 @Component({
@@ -11,10 +11,8 @@ export class PersonajesComponent {
   // @Input() personajes: any[] = [];//esto quiere decir que 'personajes' van a venir desde el componente padre
 
   get personajes() {//al llamarse asi , la vista html podra utilizarla como si hiciera uso de una propiedad,pero sigue siendo un getter
-    return this.dbzService.personajes;//hacemos uso del metodo get personajes definido en el servicio
-    
+    return this.dbzService.personajes;//hacemos uso del metodo get personajes definido en el servicio 
   }
-
 
   constructor( private dbzService: DbzService) {}//esto se como injeccion de depedencias, injectamos el servicio en este componente
 }
