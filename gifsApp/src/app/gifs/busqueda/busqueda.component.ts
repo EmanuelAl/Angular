1aebe1a67ed1e6fa943380de6347719295c6b057
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-04-07 23:40:11 -0300

    explicacion de seleccionar elementos de la vista con @ViewChild

--- gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent{
 
-  constructor() { }
+//   {static: true/false} , es true si la vista(html) no depende de ningun condicional
+//   es false si depende la vista(html) de alguna condicional*/
+// @ViewChild('textos',{static: true}) textos: ElementRef | any//para utilizar la seleccion de la vista
 
-  ngOnInit(): void {
+
+
+  //     input referencia nombre prop 
+  @ViewChild('txtBuscar', {static: true}) txtBuscar?: ElementRef<HTMLInputElement> | any;
+
+  buscar() {
+
+    const valor = this.txtBuscar?.nativeElement.value;
+
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
+/*
+el @ViewChild() es como usar en javascript el document.querySelector('etiqueta')
+es para usar lo como selector
+
+//en angular para seleccionar algun elemento del DOM podemos usar los viewchilds:
+
+    -- en la vista creamos la variable a seleccionar con #nombrevariable
+    -- en el componente se importa la clase ViewChild
+    -- se crea en el componente una propiedad nueva con el decorador @ViewChild('nombrevariable')
+
+
+operador not null:
+lo usamos para indicar a typescript que estamos 100%
+que la propiedad va a tener un valor siempre
+que no va a tener en algun momento un valor null
+el operador es ?: se pone despues entre el nombre
+de la propiedad y el tipo 
+
+@ViewChild('txtBuscar') txtBuscar?: ElementRef;
+
+
+
+*/
