e4661f06f71ba8e92765412c8e5ba07fedb953a5
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-04-10 02:56:57 -0300

    se elimina lo que no sirve en el codigo

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -57,7 +57,7 @@ export class GifsService {
 
     this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
              .subscribe( (resp ) => {
-                console.log(resp.data);
+                // console.log(resp.data);
                 this.resultados = resp.data;
                 localStorage.setItem('resultados', JSON.stringify(this.resultados));
              });
