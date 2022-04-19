050d965fe11aa0448e4ac0672b2f749b66bd064a
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-04-19 11:57:52 -0300

    se hace el manejo de errores en las peticiones (desde el lado del componente que consume el servicio de PaisService

--- paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -9,18 +9,22 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
   termino: string = '';
+  hayError: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
 
     this.paisService.buscarPais(this.termino).subscribe(
-      resp => {
+      (resp) => {
         console.log(resp);
-      }
-    );
-
+      },(err) => {
+        // console.log('Error');
+        // console.info(err);
+        this.hayError = true;
+      });
 
   }
  
--- paisesApp/src/app/pais/services/pais.service.ts
+++ paisesApp/src/app/pais/services/pais.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,6 @@ export class PaisService {
     const url = `${ this.apiUrl }/name/${ termino }`;
 
     return this.http.get( url );
-
   }
 
 }
