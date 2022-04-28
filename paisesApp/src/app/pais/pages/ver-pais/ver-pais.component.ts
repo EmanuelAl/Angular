7ac033c82f7750f97404cbf019ddb84dc0256f05
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-04-27 23:23:15 -0300

    desarrollando la vista del componente ver-pais, la vista del pais detallado

--- paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';//nos permite recibir un observable y regresar otro observable
+import { switchMap, tap } from 'rxjs/operators';//nos permite recibir un observable y regresar otro observable
 
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -12,17 +13,20 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;//!: ese simbolo le dice typescript que sabemos lo que hacemos, que puede ser nulo,pero que lo trate como si siempre tuviera data(algun valor)
+
   constructor(private activatedRoute: ActivatedRoute,
               private paisService: PaisService) { }
 
   ngOnInit(): void {
 
     this.activatedRoute.params
-    .pipe(
-      switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) )
+    .pipe(//metodo pipe() que me permite transformar la salida del subscribe
+      switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) ),
+      tap(console.log)// el tap se usa para disparar un efecto secundario, aca recibe el producto del swichMap y ejecuta el console.log para mostrarlo en consola
     )
-    .subscribe( resp => {
-      console.log(resp);
+    .subscribe( pais => {
+      this.pais = pais;
     });
 
     //otra forma:
