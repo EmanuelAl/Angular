eae1d65b80adf6252c3c8164394887a5eac52c77
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-05-02 23:42:01 -0300

    se implementan nuevas formas de agregar clases de css condicionales en el componente por-region

--- paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -16,6 +16,12 @@ export class PorRegionComponent  {
   regionActiva: string = '';
   constructor() { }
 
+  getClaseCSS( region: string): string {
+    return ( region === this.regionActiva) 
+               ? 'btn btn-primary'
+               : 'btn btn-outline-primary';
+  }
+
   activarRegion( region: string){
     this.regionActiva = region;
 
