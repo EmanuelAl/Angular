9474f0ce878a4c1206fd0a3f1bbb84723b99fe6a
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-05-02 22:49:23 -0300

    se desarrolla la vista en el componente por region,se aprende a agregar clases a elemento html bajo una condicion dada

--- paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent  {
 
+  regiones: string[] = ['africa','américa', 'asia', 'europa', 'oceanía'];
+  regionActiva: string = '';
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegion( region: string){
+    this.regionActiva = region;
+
+    //TODO hacer el llamado al servicio
   }
 
 }
